c26d4135ad32671c0cefd9bd621fdf5e49e4a5d7
L.P <parisot.laurent@gmail.com>
2017-09-15 09:56:52 +0200

    Populate database with spring JDBC and add query methods to repositories

--- src/main/java/com/lpa/spring5recipeapp/controllers/IndexController.java
+++ src/main/java/com/lpa/spring5recipeapp/controllers/IndexController.java
@@ -1,13 +1,33 @@
 package com.lpa.spring5recipeapp.controllers;
 
+import com.lpa.spring5recipeapp.domain.Category;
+import com.lpa.spring5recipeapp.domain.UnitOfMeasure;
+import com.lpa.spring5recipeapp.repositories.CategoryRepository;
+import com.lpa.spring5recipeapp.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Optional;
+
 @Controller
 public class IndexController {
+
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage() {
-        System.out.println("get index page!");
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("cat id is " + categoryOptional.get().getId());
+        System.out.println("uom id is " + unitOfMeasureOptional.get().getId());
+
         return "index";
     }
 }

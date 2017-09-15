9f02fa719155160b04dbdb4cf994693baa23e41c
L.P <parisot.laurent@gmail.com>
2017-09-15 09:58:20 +0200

    Populate database with spring JDBC and add query methods to repositories

--- src/main/java/com/lpa/spring5recipeapp/repositories/CategoryRepository.java
+++ src/main/java/com/lpa/spring5recipeapp/repositories/CategoryRepository.java
@@ -7,5 +7,6 @@ import java.util.Optional;
 
 public interface CategoryRepository extends CrudRepository<Category, Long> {
 
+    // JPA will implements automatically the SQL part
     Optional<Category> findByDescription(String description);
 }

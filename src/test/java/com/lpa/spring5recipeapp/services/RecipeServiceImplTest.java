1c89cee46802734961bf7bd8e51e74621ab72c99
L.P <parisot.laurent@gmail.com>
2017-09-19 11:03:24 +0200

    JUnit test and mockito

---
+++ src/test/java/com/lpa/spring5recipeapp/services/RecipeServiceImplTest.java
@@ -0,0 +1,45 @@
+package com.lpa.spring5recipeapp.services;
+
+import com.lpa.spring5recipeapp.domain.Recipe;
+import com.lpa.spring5recipeapp.repositories.RecipeRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class RecipeServiceImplTest {
+
+    RecipeService recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        // tell mockito to give me a mock recipe repository
+        MockitoAnnotations.initMocks(this);
+
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    public void getRecipes() throws Exception {
+        Recipe recipe = new Recipe();
+        HashSet recipesData = new HashSet();
+        recipesData.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(recipesData);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+
+        assertEquals(1, recipes.size());
+        verify(recipeRepository, times(1)).findAll();
+    }
+
+}

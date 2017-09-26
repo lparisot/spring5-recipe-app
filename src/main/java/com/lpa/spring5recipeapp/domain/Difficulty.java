47f2bfa13b735799c58d9c91de090f721390d0c3
L.P <parisot.laurent@gmail.com>
2017-09-26 16:50:38 +0200

    Enum dropdowns

--- src/main/java/com/lpa/spring5recipeapp/domain/Difficulty.java
+++ src/main/java/com/lpa/spring5recipeapp/domain/Difficulty.java
@@ -1,14 +1,6 @@
 package com.lpa.spring5recipeapp.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 
-@Getter
-@AllArgsConstructor
 public enum Difficulty {
-    EASY("1", "EASY"), MODERATE("2", "MODERATE"), HARD("3", "HARD");
-    private final String value;
-    private final String text;
-
-    public Difficulty[] getValues() { return Difficulty.values(); }
+    EASY, MODERATE, HARD
 }

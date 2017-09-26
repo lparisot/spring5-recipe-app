35153ddebb1542f8161e69716c9b0ae523fed716
L.P <parisot.laurent@gmail.com>
2017-09-26 15:51:05 +0200

    Delete an ingredient

--- src/main/java/com/lpa/spring5recipeapp/controllers/IngredientController.java
+++ src/main/java/com/lpa/spring5recipeapp/controllers/IngredientController.java
@@ -92,4 +92,15 @@ public class IngredientController {
 
         return "recipe/ingredient/ingredientform";
     }
+
+    @GetMapping
+    @RequestMapping("/recipe/{recipeId}/ingredient/{ingredientId}/delete")
+    public String deleteIngredient(@PathVariable String recipeId,
+                                   @PathVariable String ingredientId) {
+        log.debug("deleting ingredient id:" + ingredientId);
+
+        ingredientService.deleteById(Long.valueOf(recipeId), Long.valueOf(ingredientId));
+
+        return "redirect:/recipe/" + recipeId + "/ingredients";
+    }
 }
--- src/main/java/com/lpa/spring5recipeapp/services/IngredientServiceImpl.java
+++ src/main/java/com/lpa/spring5recipeapp/services/IngredientServiceImpl.java
@@ -108,6 +108,32 @@ public class IngredientServiceImpl implements IngredientService {
 
     @Override
     public void deleteById(Long recipeId, Long ingredientIdToDelete) {
+        log.debug("Deleting ingredient: " + recipeId + " : " + ingredientIdToDelete);
 
+        Optional<Recipe> recipeOptional = recipeRepository.findById(recipeId);
+
+        if(recipeOptional.isPresent()){
+            Recipe recipe = recipeOptional.get();
+            log.debug("Found recipe");
+
+            Optional<Ingredient> ingredientOptional = recipe
+                    .getIngredients()
+                    .stream()
+                    .filter(ingredient -> ingredient.getId().equals(ingredientIdToDelete))
+                    .findFirst();
+
+            if(ingredientOptional.isPresent()) {
+                log.debug("Found Ingredient");
+                Ingredient ingredientToDelete = ingredientOptional.get();
+                ingredientToDelete.setRecipe(null);
+                recipe.getIngredients().remove(ingredientOptional.get());
+                recipeRepository.save(recipe);
+            } else {
+                log.debug("Does not found ingredient " + ingredientIdToDelete);
+            }
+
+        } else {
+            log.debug("Recipe Id Not found. Id:" + recipeId);
+        }
     }
 }

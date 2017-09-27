53d26c48daefb6417978cffcee54c18c54b94609
L.P <parisot.laurent@gmail.com>
2017-09-27 09:14:23 +0200

    Prepare persisting images to database

--- src/main/java/com/lpa/spring5recipeapp/services/ImageServiceImpl.java
+++ src/main/java/com/lpa/spring5recipeapp/services/ImageServiceImpl.java
@@ -1,5 +1,6 @@
 package com.lpa.spring5recipeapp.services;
 
+import com.lpa.spring5recipeapp.repositories.RecipeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -7,6 +8,12 @@ import org.springframework.web.multipart.MultipartFile;
 @Slf4j
 @Service
 public class ImageServiceImpl implements ImageService {
+    private final RecipeRepository recipeRepository;
+
+    public ImageServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
     @Override
     public void saveImageFile(Long recipeId, MultipartFile file) {
         log.debug("Received a file");

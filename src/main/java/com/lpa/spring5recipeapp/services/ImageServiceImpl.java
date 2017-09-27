e16d5b30a6970a4fafd56e290b43b2b6291e8a0b
L.P <parisot.laurent@gmail.com>
2017-09-27 09:24:25 +0200

    Persisting images to database

--- src/main/java/com/lpa/spring5recipeapp/services/ImageServiceImpl.java
+++ src/main/java/com/lpa/spring5recipeapp/services/ImageServiceImpl.java
@@ -1,9 +1,12 @@
 package com.lpa.spring5recipeapp.services;
 
+import com.lpa.spring5recipeapp.domain.Recipe;
 import com.lpa.spring5recipeapp.repositories.RecipeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Slf4j
 @Service
@@ -17,5 +20,26 @@ public class ImageServiceImpl implements ImageService {
     @Override
     public void saveImageFile(Long recipeId, MultipartFile file) {
         log.debug("Received a file");
+
+        try {
+            Recipe recipe = recipeRepository.findById(recipeId).get();
+
+            Byte[] byteObjects = new Byte[file.getBytes().length];
+
+            int i = 0;
+
+            for (byte b : file.getBytes()) {
+                byteObjects[i++] = b;
+            }
+
+            recipe.setImage(byteObjects);
+
+            recipeRepository.save(recipe);
+        } catch (IOException e) {
+            //todo handle better
+            log.error("Error occurred", e);
+
+            e.printStackTrace();
+        }
     }
 }

9109ff37f60c04b900ca46d71e676cde142cce45
L.P <parisot.laurent@gmail.com>
2017-09-27 13:09:29 +0200

    Use global Controller advice to manage number format exception

--- src/main/java/com/lpa/spring5recipeapp/controllers/ControllerExceptionHandler.java
+++ src/main/java/com/lpa/spring5recipeapp/controllers/ControllerExceptionHandler.java
@@ -1,7 +1,29 @@
 package com.lpa.spring5recipeapp.controllers;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
 
+@Slf4j
 @ControllerAdvice
 public class ControllerExceptionHandler {
+
+    // Globally handle some exceptions
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(NumberFormatException.class)
+    public ModelAndView handleNumberFormat(Exception exception) {
+        log.error("Handling NumberFormatException");
+        log.error(exception.getMessage());
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("error/400error");
+        modelAndView.addObject("exception", exception);
+
+        return modelAndView;
+    }
 }
